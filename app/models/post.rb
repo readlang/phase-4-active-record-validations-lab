40a6105c1cf6090f2206521a69a0ad7e3a058e0b
Read Langworthy <rlangworthy@gmail.com>
2022-04-12 16:29:38 -0700

    commit tree 2fcc09136e0319cf12e77cf385c2256b006caa11

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    # validates :title, comparison: { other_than: "True Facts" }
+
+    validates :title, exclusion: {in: ["True Facts"]}
+
 end
